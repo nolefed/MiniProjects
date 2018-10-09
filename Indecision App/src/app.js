9df65c36d74c68e9b95c4598bd0e863e22215565
nolefed <42758645+nolefed@users.noreply.github.com>
2018-10-09 10:14:04 -0400

    Adding files related to Indecision App

--- Indecision App/src/app.js
+++ Indecision App/src/app.js
@@ -1,84 +1,10 @@
-class IndecisionApp extends React.Component{
-    render(){
-        const title= 'Indecision';
-        const subtitle ='Put your life in the hands of a computer.';
-        const options = ['Thing one', 'Thing two', 'Thing three'];
-        return(
-            <div>
-            <Header title= {title} subtitle= {subtitle} />
-            <Action />
-            <Options options={options} />
-            <AddOption />
-            </div>
-        );
-    }
-}
-
-class Header extends React.Component {
-    render(){
-        return (
-            <div>
-            <h1>{this.props.title}</h1>
-            <h2>{this.props.subtitle}</h2>
-            </div>
-        );
-    }
-}
-
-class Action extends React.Component{
-    handlePick(){
-        alert('handlePick');
-    }
-    render(){
-        return (
-            <button onClick={this.handlePick}> What should I do </button>
-        );
-    }
-}
-
-class Options extends React.Component{
-    handleRemoveAll(){
-        alert('Remove All')
-    }
-    render(){
-        return(
-            <div>
-            <button onClick={this.handleRemoveAll}> Remove All </button>
-           {
-            this.props.options.map((option) => <Option key={option} optionText={option}/>)
-           }
-            </div>
-        );
-    }
-}
-
-class Option extends React.Component{
-    render(){
-        return(
-            <div>
-            {this.props.optionText}
-            </div>
-        );
-    }
-}
-
-class AddOption extends React.Component{
-    handleAddOption(e){
-        e.preventDefault();
-        const option = e.target.elements.option.value.trim();
-        if(option){
-            alert(option);
-        }
-    }
-    render(){
-        return(
-            <form onSubmit= {this.handleAddOption}>
-             <input type= "text" name= "option"/>
-             <button>Add Option</button>
-            </form>
-        );
-    }
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp'
+import 'normalize.css/normalize.css'; 
+import './styles/styles.scss'
+ 
 
 
-ReactDOM.render(<IndecisionApp />,document.getElementById('app'));
+ReactDOM.render(<IndecisionApp  />,document.getElementById('app'));
+
--- Indecision App/src/playground/build-it-visible.js
+++ Indecision App/src/playground/build-it-visible.js
@@ -1,30 +1,33 @@
-const app = {
-title: 'Visibility Toggle'
-};
+class VisibilityToggle extends React.Component{
+    constructor(props){
+        super(props);
+        this.handleToggleVisibility=this.handleToggleVisibility.bind(this);
+        this.state ={
+            visibility:false
+        }
+    }
+    handleToggleVisibility(){
+        this.setState((prevState)=> {
+            return{
+                visibility: !prevState.visibility
+            };
+        });
+    }
+    render(){
+        return(
+         <div>
+         <h1> Visibility Toggle</h1>
+         <button onClick={this.handleToggleVisibility}> {this.state.visibility ? 'Hide Details' : 'Show Details'} </button>
+         {this.state.visibility && (
+             <div>
+             <p> Hey These are some details you can now see </p>
+             </div>
+         )}
+         </div>   
+        );
+    }
 
-let visibility =false;
-
-const appRoot = document.getElementById('app');
-
-const toggleVisibility = () =>{
-    visibility=!visibility;
-    render();
 }
 
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
 
-const render = () =>{
-    const template =(
-        <div>
-        <h1> {app.title}</h1>
-        <button onClick={toggleVisibility}> {visibility ? 'Hide Details' : 'Show Details'} </button>
-        {visibility && (
-            <div>
-            <p> Hey These are some details you can now see </p>
-            </div>
-        )}
-        </div>
-    )
-    ReactDOM.render(template, appRoot);
-};
-
-render();
