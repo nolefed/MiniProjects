9c52294f3d08b539689d7da5120eb01f5899d5ac
nolefed <42758645+nolefed@users.noreply.github.com>
2018-11-02 10:06:37 -0400

    updating files

--- Java Concept Based Programs/Sets and HashSets/src/com/bhusalmanish/SetMain.java
+++ Java Concept Based Programs/Sets and HashSets/src/com/bhusalmanish/SetMain.java
@@ -7,45 +7,85 @@ import java.util.Set;
 public class SetMain {
     public static void main(String[] args) {
 
-        Set<Integer> squares=new HashSet<>();
+        Set<Integer> squares = new HashSet<>();
         Set<Integer> cubes = new HashSet<>();
 
-        for(int i=1;i<=100;i++){
+        for (int i = 1; i <= 100; i++) {
 
-            squares.add(i*i);
-            cubes.add(i*i*i);
+            squares.add(i * i);
+            cubes.add(i * i * i);
 
         }
 
-        System.out.println("There are " + squares.size()+ " squares and " +cubes.size()+" cubes." );
+        System.out.println("There are " + squares.size() + " squares and " + cubes.size() + " cubes.");
         Set<Integer> union = new HashSet<>(squares);
         union.addAll(cubes);
-        System.out.println("Union contains "+union.size()+" elements");
+        System.out.println("Union contains " + union.size() + " elements");
 
         Set<Integer> intersection = new HashSet<>(squares);
         intersection.retainAll(cubes);
-        System.out.println("Intersection contains "+intersection.size()+ " elements");
+        System.out.println("Intersection contains " + intersection.size() + " elements");
 
-        for(int i: intersection){
-            System.out.println(i+ " is the square of " + Math.sqrt(i) + " and the cube of "+Math.cbrt(i));
+        for (int i : intersection) {
+            System.out.println(i + " is the square of " + Math.sqrt(i) + " and the cube of " + Math.cbrt(i));
         }
 
-        Set<String> words= new HashSet<>();
+        Set<String> words = new HashSet<>();
         String sentence = " One day in the year of the fox";
-        String [] arrayWords = sentence.split(" ");
+        String[] arrayWords = sentence.split(" ");
         words.addAll(Arrays.asList(arrayWords));
-        for(String s: words){
+        for (String s : words) {
             System.out.println(s);
         }
         Set<String> nature = new HashSet<>();
         Set<String> divine = new HashSet<>();
-        String[] natureWords ={"all", "nature", "is", "but", "art", "unknown", "to", "thee"};
-        String[] divineWords={"to","err","is","human","to", "forgive","divine"};
+        String[] natureWords = {"all", "nature", "is", "but", "art", "unknown", "to", "thee"};
+        nature.addAll(Arrays.asList(natureWords));
 
+        String[] divineWords = {"to", "err", "is", "human", "to", "forgive", "divine"};
+        divine.addAll(Arrays.asList(divineWords));
 
+        System.out.println("nature - divine: ");
+        Set<String> diff1 = new HashSet<>(nature);
+        diff1.removeAll(divine);
+        printSet(diff1);
+
+        System.out.println("divine - nature");
+        Set<String> diff2 = new HashSet<>(divine);
+        diff2.removeAll(nature);
+        printSet(diff2);
+
+        // symmetric difference is removing intersection(common part between two sets ) from union
+
+        Set<String> unionTest=new HashSet<>(nature);
+        unionTest.addAll(divine);      // contains union, that is element from nature and divine and common elements, but no duplicates
+        Set<String> intersectionTest=new HashSet<>(nature);
+        intersectionTest.retainAll(divine);  // contains common part between nature and divine
+
+        System.out.println("Symmetric difference ");
+        unionTest.removeAll(intersectionTest);
+        printSet(unionTest);
+
+        if(nature.containsAll(divine)){
+            System.out.println("Divine is a subset of nature");
+        }
+        if(nature.containsAll(intersectionTest)){
+            System.out.println("Intersection is a subset of nature");
+        }
+        if(divine.containsAll(intersectionTest)){
+            System.out.println("intersection is a subset of divine");
+        }
 
 
 
 
     }
-}
+        private static void printSet(Set<String> set){
+            System.out.print("\t");
+            for(String s:set){
+                System.out.print(s+ " ");
+            }
+            System.out.println();
+        }
+    }
+
